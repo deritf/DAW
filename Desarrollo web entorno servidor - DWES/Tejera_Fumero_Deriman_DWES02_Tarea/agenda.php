16fb36d099f812fd52c8ae2d32f71896aa41e174
Derimán Tejera <deriman.tejera@gmail.com>
2024-10-01 02:29:00 +0100

    Subido de nuevo DWES02
    Añade informe con el código y demostración en capturas.

---
+++ Desarrollo web entorno servidor - DWES/Tejera_Fumero_Deriman_DWES02_Tarea/agenda.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['agenda'])) {
+    $_SESSION['agenda'] = [];
+}
+
+if (isset($_POST['nombre']) && isset($_POST['telefono'])) {
+    $nombre = trim($_POST['nombre']);
+    $telefono = trim($_POST['telefono']);
+
+    $nombre=ucwords(strtolower($nombre));
+
+    if (empty($nombre)) {
+
+        $_SESSION['mensaje'] = "Introducir un nombre...";
+    } else {
+        if (isset($_SESSION['agenda'][$nombre])) {
+            if (!empty($telefono)) {
+                $_SESSION['agenda'][$nombre] = $telefono;
+                $_SESSION['mensaje'] =  "Contacto actualizado...";
+            } else {
+                unset($_SESSION['agenda'][$nombre]);
+                $_SESSION['mensaje'] =  "Contacto eliminado...";
+            }
+        } else {
+            if (!empty($telefono)) {
+                $_SESSION['agenda'][$nombre] = $telefono;
+                $_SESSION['mensaje'] =  "Contacto añadido...";
+            } else {
+                $_SESSION['mensaje'] =  "Introduzca un número de teléfono...";
+            }
+        }
+    }
+}
+
+if (isset($_GET['vaciar'])) {
+    $_SESSION['agenda'] = [];
+    $_SESSION['mensaje'] =  "Agenda vaciada...";
+}
+
+header('Location: index.php');
+exit();
+?>
+
+# Autor: Derimán Tejera Fumero.
+# Asignatura: DWES CFGS DAW
+# Fecha: 27-09-2024
